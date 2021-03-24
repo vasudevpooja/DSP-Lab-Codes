411d412de36dfb9f443d6f5d616fb0871df0c9cd
Pooja Vasudev <poojavasudev.oct08@gmail.com>
2021-03-24 20:33:14 +0530

    Code for IDFT

--- INDFT_FUNCTION.m
+++ INDFT_FUNCTION.m
@@ -7,3 +7,29 @@ function a = INDFT_FUNCTION(y, N)
     a = (1/N).*(y*W);
     a= a';
 return
+
+%clc;
+%clear all;
+%close all;
+%Xk=input('Enter the input sequence');
+%xn=calcidft(Xk);
+%N=length(xn);
+%disp('x(n)=');
+%disp(xn);
+%n=0:N-1;
+%stem(n,xn);
+%title('IFFT Sequence');
+%xlabel('Time');
+%ylabel('Amplitude');
+%function [xn]=calcidft(Xk)
+%N=length(Xk);
+%for k=0:1:N-1;
+%for n=0:1:N-1;
+%p=exp(i*2*pi*n*k/N);
+%IT(k+1,n+1)=p;
+%end
+%end
+%disp('Transformation Matrix for IDFT')
+%disp(IT);
+%xn=(IT*(Xk.'))./N;
+%end
